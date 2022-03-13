9dbf62bf988d285960ccb4497120123c0992ab73
Emmanuel <oluwatopealofe@gmail.com>
2022-03-13 10:53:13 -0400

    Changed repr of board

--- tictactoe.py
+++ tictactoe.py
@@ -7,21 +7,21 @@ class Board:
             self.board.append([' '] * 3)
     
     def __repr__(self):
-        print(" ----- ----- ----- ")
-        print("|     |     |     |")
-        print("|  {0}  |  {1}  |  {2}  |".format(self.board[0][0], self.board[0][1], self.board[0][2]))
-        print("|     |     |     |")
-        print(" ----- ----- ----- ")
-        print("|     |     |     |")
-        print("|  {0}  |  {1}  |  {2}  |".format(self.board[1][0], self.board[1][1], self.board[1][2]))
-        print("|     |     |     |")
-        print(" ----- ----- ----- ")
-        print("|     |     |     |")
-        print("|  {0}  |  {1}  |  {2}  |".format(self.board[2][0], self.board[2][1], self.board[2][2]))
-        print("|     |     |     |")
-        print(" ----- ----- ----- ")
-        return ""
-
+        l1 = " ----- ----- ----- \n"
+        l2 = "|     |     |     |\n"
+        l3 = "|  {0}  |  {1}  |  {2}  |\n".format(self.board[0][0], self.board[0][1], self.board[0][2])
+        l4 = "|     |     |     |\n"
+        l5 = " ----- ----- ----- \n"
+        l6 = "|     |     |     |\n"
+        l7 = "|  {0}  |  {1}  |  {2}  |\n".format(self.board[1][0], self.board[1][1], self.board[1][2])
+        l8 = "|     |     |     |\n"
+        l9 = " ----- ----- ----- \n"
+        l10 = "|     |     |     |\n"
+        l11 = "|  {0}  |  {1}  |  {2}  |\n".format(self.board[2][0], self.board[2][1], self.board[2][2])
+        l12 = "|     |     |     |\n"
+        l13 = " ----- ----- ----- \n"
+        return l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11 + l12 + l13
+    
     def play_turn(self, coordinates, player):
         rows_played = {0:0, 1:0, 2:0} # Store number of entries in each row
         cols_played = {0:0, 1:0, 2:0} # Store number of entries in each column
@@ -52,15 +52,14 @@ class Board:
         return 1 # Return 1 if play was valid and not a winning one
 
 '''
-Tests:
+# Tests
 test_board = Board()
 test = test_board.play_turn("00", 'x')
-print(test)
+print(test_board)
 test = test_board.play_turn("11", 'x')
-print(test)
+print(test_board)
 test = test_board.play_turn("00", 'x')
-print(test)
+print(test_board)
 test = test_board.play_turn("22", 'x')
-print(test)
 print(test_board)
 '''
